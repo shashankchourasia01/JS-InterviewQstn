1ae14309d95f60401a6919fde697c1de8c676cab
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-19 20:04:11 +0530

    %operator

---
+++ oddEven!==%.js
@@ -0,0 +1,12 @@
+// find odd even num without using % operator
+
+
+function oddEven(n){
+    if(Math.floor(n/2) * 2 === n){
+        console.log("even");
+    } else{
+        console.log("odd");
+        
+    }
+}
+oddEven(7)
