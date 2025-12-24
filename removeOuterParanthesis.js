392d087a7a132240d214558b2a46ebb3bfa57511
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-24 19:50:27 +0530

    remove outer paranthesis str

---
+++ removeOuterParanthesis.js
@@ -0,0 +1,21 @@
+function remove(s){
+    let counter = 0;
+    let result = "";
+    for ( let i = 0; i < s.length; i++){
+        const char = s[i]
+        if(char === '('){
+            if( counter > 0){
+                result += char;
+            }
+            counter ++;
+        }
+        else if ( char === ')'){
+            counter --
+            if( counter > 0){
+                result += char
+            }
+        }
+    }
+    return result
+}
+console.log(remove('(()())(()())'));
