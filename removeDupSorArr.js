1f84930996f55c0e8b2d9ab4a14793b8e0600096
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-27 23:00:32 +0530

    count and revove dup arr

--- removeDupSorArr.js
+++ removeDupSorArr.js
@@ -3,6 +3,11 @@ function remDup(arr){
 
     let i = 0;
     for( let j = 1; j < arr.length; j++){
-        
+        if( arr[j] !== arr[i]){
+            i++;
+            arr[i] = arr[j];
+        }
     }
+    return i + 1;
 }
+console.log(remDup([1,1,2,2,3,4,4,5]));
