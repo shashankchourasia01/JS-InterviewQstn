37826fb4c626b47ec777f2b5c96ebf9c26f222e7
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-01-03 16:04:23 +0530

    fabi

---
+++ fabinacci.js
@@ -0,0 +1,11 @@
+function fabi(n){
+    let a = 0, b = 1;
+    for ( let i = 0; i < n; i++){
+        console.log(a);
+        let next = a + b;
+        a = b;
+        b = next
+        
+    }
+}
+fabi(5)
