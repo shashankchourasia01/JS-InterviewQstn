6fe4114e4287254d0e160a1ddb6cc036d871b422
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-01-06 16:02:44 +0530

    sumOf

--- sumOfDigitrs.js
+++ sumOfDigitrs.js
@@ -1,1 +1,11 @@
-func
+function sumDigits(num){
+    let sum = 0;
+    while( num > 0){
+        let digits = num % 10;
+        sum = sum + digits;
+        num = Math.floor( num / 10)
+    }
+    return num
+}
+
+console.log();
