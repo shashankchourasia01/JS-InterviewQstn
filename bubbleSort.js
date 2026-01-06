1e6581b7327dea2ef503ca3b348196e1e6e79c76
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-01-06 15:03:47 +0530

    bs

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,4 +1,16 @@
 function bS(arr){
     let n = arr.length;
-    for
+    for (let i = 0; i < n - 1; i ++){
+        for( let j = 0; j < n - i - 1; j++){
+            if( arr[j] > arr[j + 1]){
+                let temp = arr[j];
+                arr[j] = arr[ j + 1];
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr
+
 }
+
+console.log();
